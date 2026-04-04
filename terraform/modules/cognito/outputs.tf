721a67aa6c0733a5aa54e09ff4a295ce82ced971
Dawid <dbembnista1@gmail.com>
2026-04-04 23:28:29 +0200

    update

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -6,6 +6,6 @@ output "client_id" {
   value = aws_cognito_user_pool_client.client.id
 }
 
-output "cognito_domain" {L
+output "cognito_domain" {
   value = "${aws_cognito_user_pool_domain.main.domain}.auth.eu-central-1.amazoncognito.com"
 }
