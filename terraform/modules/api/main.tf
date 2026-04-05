ef8649ee199788e37fa4ad4fb5b55c4b00de477b
Dawid <dbembnista1@gmail.com>
2026-04-05 23:35:00 +0200

    update

--- terraform/modules/api/main.tf
+++ terraform/modules/api/main.tf
@@ -150,12 +150,102 @@ resource "aws_lambda_permission" "apigw_invoke_car" {
 }
 
 
+# CORS (Cross-Origin Resource Sharing)
+# ==========================================
+# CORS (Cross-Origin Resource Sharing)
+# ==========================================
+
+#  models endpoint
+resource "aws_api_gateway_method" "models_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.models.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "models_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.models.id
+  http_method = aws_api_gateway_method.models_options.http_method
+  type        = "MOCK"
+  request_templates = { "application/json" = "{\"statusCode\": 200}" }
+}
+
+resource "aws_api_gateway_method_response" "models_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.models.id
+  http_method = aws_api_gateway_method.models_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "models_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.models.id
+  http_method = aws_api_gateway_method.models_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [aws_api_gateway_integration.models_options_integration]
+}
+
+# car endpoint
+resource "aws_api_gateway_method" "car_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.car.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "car_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.car.id
+  http_method = aws_api_gateway_method.car_options.http_method
+  type        = "MOCK"
+  request_templates = { "application/json" = "{\"statusCode\": 200}" }
+}
+
+resource "aws_api_gateway_method_response" "car_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.car.id
+  http_method = aws_api_gateway_method.car_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "car_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.car.id
+  http_method = aws_api_gateway_method.car_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [aws_api_gateway_integration.car_options_integration]
+}
+
 # Deploy api gw
 
+# Deploy api gw
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.models_get_integration,
-    aws_api_gateway_integration.car_get_integration
+    aws_api_gateway_integration.car_get_integration,
+    aws_api_gateway_integration.models_options_integration,
+    aws_api_gateway_integration.car_options_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.api.id
 
@@ -163,6 +253,10 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     redeployment = sha1(jsonencode([
       aws_api_gateway_integration.models_get_integration.id,
       aws_api_gateway_integration.car_get_integration.id,
+      aws_api_gateway_integration.models_options_integration.id,
+      aws_api_gateway_integration.car_options_integration.id,
+      aws_api_gateway_integration_response.models_options_integration_response.id,
+      aws_api_gateway_integration_response.car_options_integration_response.id
     ]))
   }
 
@@ -170,6 +264,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     create_before_destroy = true
   }
 }
+
 
 resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
